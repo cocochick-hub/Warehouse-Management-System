3f3b8ac82f4fa78316e0b2cc0acf433c059c8ac4
christophen1 <2241037291@qq.com>
2026-06-24 13:13:39 +0800

    feat: 初始化 uni-app 手机端项目脚手架

---
+++ mobile/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onLaunch } from '@dcloudio/uni-app'
+import { useUserStore } from './store/user'
+
+onLaunch(() => {
+  const userStore = useUserStore()
+  userStore.checkLogin()
+})
+</script>
+
+<style lang="scss">
+@import 'vant/lib/index.css';
+
+page {
+  background-color: #f7f8fa;
+  font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', sans-serif;
+}
+</style>
